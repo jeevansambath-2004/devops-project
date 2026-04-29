1f3c6e4d50d88b76f65479fbc0cdf9ff6d347d2e
jeevansambath-2004 <s99577939@gmail.com>
2026-04-29 22:12:23 +0530

    fix: resolve absolute path RollupError in Jenkins

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: fs.realpathSync(process.cwd()),
   plugins: [react()],
 })
